d640468b63513f65f1a7b92a611c6b6d689cde98
garvi <plastictumor@gmail.com>
2022-09-20 21:05:10 -0400

    added print statement abt fav food

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("hi");
         System.out.println("friday is my favorite movie");
+        System.out.println("my favorite food is pizza");
     }
 }
